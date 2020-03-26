c216e0ec482ce8ee0fcc8bbaca97ee2ade1ea9a7
Warren Harper II <warrenharper2@att.net>
2020-03-26 15:19:47 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Post.create(title: 'A lovely title', description: 'A superb description')
+Post.create(title: 'A lovely title', description: 'A superb description'),
+Post.create(title: 'Loys Book', description: 'Welcome to my World')
